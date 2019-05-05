aa941408586fac7f1b14157799c3e4b77bbb5802
Jeff Hill <jeff@reverentengineer.com>
2019-05-05 19:43:56 -0400

    Add html_theme_options to documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'build_dashboard'
+project = 'Build Dashboard'
 copyright = '2019, Jeff Hill'
 author = 'Jeff Hill'
 
@@ -52,3 +52,10 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options = {
+        'github_user': 'ReverentEngineer',
+        'github_repo': 'build-dashboard',
+        'travis_button': True,
+        'github_button': True
+}
