844f2c8c8b5875f38787729ec95f3a702af2b05e
Jeff Hill <jeff@reverentengineer.com>
2019-05-10 06:22:13 -0400

    Remove old test.

--- testing/test_buildbot_model.py
+++ testing/test_buildbot_model.py
@@ -11,14 +11,6 @@ class TestBuildbotModel(TestCase):
 
     @patch('build_dashboard.model.BuildbotClient')
     def test_builders(self, mockClient):
-        EXPECTED = [ { 'builderid': 1, 'name': 'builder' } ]
-        mockClient.builders = Mock(side_effect=coroutine(Mock(return_value=EXPECTED)))
-        model = BuildbotModel(mockClient)
-        builders = model.builders()
-        assert EXPECTED == builders
-
-    @patch('build_dashboard.model.BuildbotClient')
-    def test_builders(self, mockClient):
         BUILDERS = [ { 'builderid': 1, 'name': 'builder' } ]
         BUILDS = [ { 'builderid': 1, 'name': 'builder', 'buildid': 1 } ]
         EXPECTED = BUILDERS.copy()
@@ -26,5 +18,5 @@ class TestBuildbotModel(TestCase):
         mockClient.builders = Mock(side_effect=coroutine(Mock(return_value=BUILDERS)))
         mockClient.builds = Mock(side_effect=coroutine(Mock(return_value=BUILDS)))
         model = BuildbotModel(mockClient)
-        builders = model.builders_with_builds()
+        builders = model.builders()
         assert EXPECTED == builders
